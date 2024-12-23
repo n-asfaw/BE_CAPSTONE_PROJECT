49866703ca0675b69a9e6df33b4b21a8c3ceda16
n-asfaw <nardosasfaw77@gmail.com>
2024-12-23 09:57:37 +0300

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# BE_CAPSTONE_PROJECT
+
+## Setup Instructions
+
+1. Clone the repository:
+
+```bash
+git clone <repository-url>
+cd <BE_CAPSTONE_PROJECT>
---
+++ library_managment_system_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LibraryManagmentSystemApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'library_managment_system_api'
